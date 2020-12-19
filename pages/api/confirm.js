086431cc4d2525d2ce9d20eb4a0d8f7c16c4e7da
victorfrei <vic.frei2@gmail.com>
2020-12-19 13:51:08 -0300

    commit

--- pages/api/confirm.js
+++ pages/api/confirm.js
@@ -19,7 +19,7 @@ const conn = await mongoose.createConnection(`mongodb+srv://Register:${process.e
 
  const user = conn.model("users",userSchema);
 
-if(req.body.Token!=null && req.body.Token!="0001"){
+if(req.body.Token!="null" && req.body.Token!="0001"){
 jwt.verify(req.body.Token,process.env.privateKey);
 const {data:{Nick}} = jwt.decode(req.body.Token);
 const {Number,EmailVerificado} = await user.findOne({Nick});
