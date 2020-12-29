bf52aca20c7aae53ef2571f32f360fb5612a9fea
victorfrei <vic.frei2@gmail.com>
2020-12-29 17:19:12 -0300

    commit

--- pages/watch/[watch].js
+++ pages/watch/[watch].js
@@ -1,6 +1,7 @@
 import {Box} from '@chakra-ui/react';
 import {useToast,Tabs} from "@chakra-ui/react";
 import Navmenu from '../../components/navmenu';
+import ReactPlayer from 'react-player';
 
 
 export default function watch(){
@@ -20,7 +21,9 @@ export default function watch(){
       }
 
   
-       
+     
+
+
 
 return (<div>
 <Tabs w="100vw" variant="solid-rounded" align="center" colorScheme="blue">
@@ -29,7 +32,7 @@ return (<div>
  <div style={{marginLeft:"40px",marginRight:"auto",width:"65%"}}>
         
         <Box w="100%">
-            <video margin="40px" controls src="https://stream.mux.com/QoRS84f02ZbC702c6hUq01CzNLrOdmARzXkNP02018w00kTUU.m3u8"></video>
+            <ReactPlayer margin="40px" controls url="https://stream.mux.com/QoRS84f02ZbC702c6hUq01CzNLrOdmARzXkNP02018w00kTUU.m3u8"></ReactPlayer>
         </Box>
             
 </div>
