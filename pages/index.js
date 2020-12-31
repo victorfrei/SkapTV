cc28d47780135a702b3941691a42faa87134ef86
victorfrei <vic.frei2@gmail.com>
2020-12-31 11:45:32 -0300

    commmit

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,13 @@ import Channelspotlight from "../components/channelspotolight";
 import { Tabs, TabPanels, TabPanel } from "@chakra-ui/react"
 import Navmenu from "../components/navmenu";
 var ip = '<!--#echo var="REMOTE_ADDR"-->';
-
-// export async function getStaticProps(){
+console.log(ip);
+export async function getStaticProps(){
   
-//   const call = await fetch(`https://skap.tv/api/skap/listvideos?auth=${process.env.Auth_Secret}`)
-//   return {props:{LV:await call.json()},revalidate: 5}
+  const call = await fetch(`https://skap.tv/api/skap/listvideos?auth=${process.env.Auth_Secret}`)
+  return {props:{LV:await call.json()},revalidate: 5}
  
-// };
+};
 
 export default function home(props){
   const [isloaded,setisloaded] = useState(false);
