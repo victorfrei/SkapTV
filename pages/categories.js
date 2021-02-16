a5d36468a80677897a9ed8b50d8b7a375c444ba3
victorfrei <vic.frei2@gmail.com>
2021-02-16 19:51:15 -0300

    commit

--- pages/categories.js
+++ pages/categories.js
@@ -1,22 +1,29 @@
-import { Box, Flex, SimpleGrid,Image,Text, useColorModeValue, Skeleton } from "@chakra-ui/react"
+import { Box, Flex, SimpleGrid,Image,Text, useColorModeValue, Skeleton, Tag, Heading } from "@chakra-ui/react"
 import mongoose from "mongoose"
 import Navmenu from "../components/navmenu";
+import {useRouter} from 'next/router';
 
 
 export default function Category({categories}){
    const cat = JSON.parse(categories);
    const data = [];
    let loading = true;
+   const router = useRouter();
 
    for(let x=0;x<cat.length;x++){
+    let tags = [];
+    
+    cat[x].Tags.forEach(element => {
+        tags.push(<Tag cursor="pointer" bg="gray.300" color="black" onClick={()=>{router.push(`/videos?t=${element}`)}} w="fit-content">{element}</Tag>);
+    });
        data.push(
-        <Flex cursor="pointer" bg="#0f0f0f" borderRadius="20px" flexDir="column" w="220px" h="400px" alignItems="flex-start" justifyContent="center">
-            <Image borderTopRadius="20px" w="220px" h="240px" fit="cover" src={cat[x].Image}></Image>
+        <Flex  bg={useColorModeValue("#f0f0f0","#0f0f0f")} color={useColorModeValue("black","white")} borderRadius="20px" flexDir="column" w="220px" h="400px" alignItems="flex-start" justifyContent="center">
+            <Image cursor="pointer" onClick={()=>{router.push(`/videos/?q=${cat[x].Name}`)}} borderTopRadius="20px" w="220px" h="216px" fit="cover" src={cat[x].Image}></Image>
             <Flex padding="15px" w="100%" flexDir="column">
-            <Text alignSelf="center">{cat[x].Name}</Text>
+            <Heading size={3} alignSelf="center">{cat[x].Name}</Heading>
             <Text noOfLines={3}>{cat[x].Description}</Text>
             <Text>{cat[x].UsedAmount} vídeos</Text>
-            <Text>{cat[x].Tags}</Text>
+            <SimpleGrid  columns={2} spacing="10px" paddingTop="10px">{tags}</SimpleGrid>
             </Flex>
         </Flex>
        )
@@ -50,7 +57,7 @@ const categories = new mongoose.Schema(
         Description:String,
         Banner:String,
         UsedAmount:String,
-        Tags:String  
+        Tags:Array  
     }
     )
   
